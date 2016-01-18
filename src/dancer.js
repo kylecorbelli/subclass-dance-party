c77e16b0e19fc096380524c625114fcce8c736af
pacificLiving <kyle.corbelli@gmail.com>
2016-01-18 15:56:24 -0800

    Refactor blinkyDancer and dancer to use pseudoclassical instantiation style.

--- src/dancer.js
+++ src/dancer.js
@@ -2,26 +2,30 @@
 var makeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.timeBetweenSteps = timeBetweenSteps;
-  this.setPosition(top, left);
+  // this.setPosition(top, left);
   this.$node = $('<span class="dancer"></span>');
+  this.styleSettings = {};
+  this.setPosition(top, left);
   this.step();
+  // makeDancer.prototype.setPosition.call(this, top, left);
+  // makeDancer.prototype.step.call(this);
 };
 
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
-  var styleSettings = {
+  this.styleSettings = {
     top: top,
     left: left
   };
-  this.$node.css(styleSettings);
+  this.$node.css(this.styleSettings);
 };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
