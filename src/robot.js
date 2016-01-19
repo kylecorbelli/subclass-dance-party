5c442d9fd8390ade5311a1551a4bc91a30a5624f
pacificLiving <kyle.corbelli@gmail.com>
2016-01-18 20:31:56 -0800

    Set Robot z-index

--- src/robot.js
+++ src/robot.js
@@ -12,6 +12,7 @@ Robot.prototype.step = function() {
   this.xPosition === -700 ? (this.xPosition = -40) : (this.xPosition -= 220);
   var changeX = {
     'background-position' : this.xPosition + 'px' +' 0',
+    'z-index' : '-20'
   };
   //this.$node.css(changeX);
 };
