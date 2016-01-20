c816054fe4faf942e4ef530505c002eb984a95a2
pacificLiving <kyle.corbelli@gmail.com>
2016-01-19 20:01:32 -0800

    Clean-up robot code.

--- src/robot.js
+++ src/robot.js
@@ -1,7 +1,6 @@
 var Robot = function(top, left, timeBetweenSteps) {
   Dancer.call(this, $('body').height() - top/4 - 238, left, 10);
   this.$node.addClass('robot');
-  //this.xPosition = 0;
 };
 
 Robot.prototype = Object.create(Dancer.prototype);
@@ -9,10 +8,4 @@ Robot.prototype.constructor = Robot;
 
 Robot.prototype.step = function() {
   Dancer.prototype.step.call(this);
-  this.xPosition === -700 ? (this.xPosition = -40) : (this.xPosition -= 220);
-  var changeX = {
-    'background-position' : this.xPosition + 'px' +' 0',
-    'z-index' : '-20'
-  };
-  //this.$node.css(changeX);
 };
